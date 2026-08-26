eefc573b384367eded6d33e4f275f531792c6a4c
Fuad Tasin <tasinhasan183618@gmail.com>
2026-08-26 17:20:40 +0600

    A small changes on the schemas.py

--- schemas.py
+++ schemas.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel,Field
 from langchain_core.output_parsers import StrOutputParser,PydanticOutputParser
 
 class Response(BaseModel):
-    answer:str=Field(description="The answer to the user's response within 1-2 sentences.")
+    answer:str=Field(description="The answer to the user's response.Make it relavent to the question.")
     summary:str=Field(description="Give the summary of the the answer.")
     confidence:float=Field(gt=0,lt=1,description="The confidence should be between 0 to 1.")
     category:str=Field(description="The category of the user's question.The categories are Mathematical,Programming & General")
