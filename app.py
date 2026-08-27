07c506859489649420d923e31d4f1a732484017b
Fuad Tasin <tasinhasan183618@gmail.com>
2026-08-27 23:51:11 +0600

    Did some changes on the ui.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from random import SystemRandom
 from chatbot import chat_model
 from schemas import str_output_parser,pydantic_output_parser
 from langchain.messages import SystemMessage,AIMessage,HumanMessage
@@ -29,9 +30,18 @@ parallel_chain=RunnableParallel({
 if "chat_history" not in st.session_state:
     st.session_state.chat_history=[SystemMessage(content="You are a helpful assistant.Always answer useful things,try to avoid unnecessary things.And if possible then give a short paragraph(if it is wanted)")]
 
-st.header("Simple ChatBot")
+st.title("Simple AI Tutor")
 
-user_input=st.chat_input()
+for msg in st.session_state.chat_history:
+
+    if isinstance(msg,SystemMessage):
+        continue
+    elif isinstance(msg,HumanMessage):
+        st.write(msg.content,HumanMessage)
+    elif isinstance(msg,AIMessage):
+        st.write(msg.content,AIMessage)
+
+user_input=st.chat_input("Ask me anything.......")
 
 category_input=st.selectbox(
     "Response Category",
@@ -43,20 +53,17 @@ input_type=st.selectbox(
 )
 
 with st.sidebar:
-    st.subheader("Chat History")
+    st.subheader("💬 Chat History")
+    st.button("Clear",on_click=clear_text)
+    st.divider()
+    for msg in st.session_state.chat_history:
+        if isinstance(msg, SystemMessage):
+            continue
 
-    if not st.session_state.chat_history:
-        st.write("Empty")
-    else:
-        for msg in st.session_state.chat_history:
-            if isinstance(msg,SystemMessage):
-                continue
-            elif isinstance(msg,AIMessage):
-                role="AI"
-            else:
-                role="Human"
-
-            st.write(f"{role}:{msg.content}")
+        if isinstance(msg, HumanMessage):
+            st.markdown(f"**👤 You:** {msg.content}")
+        elif isinstance(msg, AIMessage):
+            st.markdown(f"**🤖 AI:** {msg.content}")
 
 if user_input:
     st.session_state.chat_history.append(HumanMessage(content=user_input))
@@ -72,9 +79,11 @@ if user_input:
     })
 
     with st.chat_message("ai"):
-        st.write(f"Summary:{result["summary"].summary}")
-        st.write(f"Result:{result["answer"].answer}")
+        st.markdown("### Summary")
+        st.write(result["summary"].summary)
+
+        st.markdown("### Answer")
+        st.write(result["answer"].answer)
 
     st.session_state.chat_history.append(AIMessage(content=result["summary"].summary))
 
-st.button("Clear",on_click=clear_text)
